bfd3adf5881a32f0f9871bf5e7f5afaf762c9603
Rodrigo Sinope 70239 <lourenco.rs@gmail.com>
2019-07-17 18:41:23 -0300

    Adiciona reducer inicial unitInfo e connection

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 
 import tabsReducer from 'features/tabs/tabsReducer';
+import unitInfoReducer from 'features/unitInfo/unitInfoReducer';
 
 import entitiesReducer from './entitiesReducer';
 
 const rootReducer = combineReducers({
   tabs: tabsReducer,
+  unitInfo: unitInfoReducer,
   entities: entitiesReducer,
 });
 
